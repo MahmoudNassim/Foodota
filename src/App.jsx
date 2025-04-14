f8a6cdd02abbf3cde6323ee44c17f5807854fd51
MahmoudNassim <mahmodnassim7@gmail.com>
2025-04-14 07:08:31 +0200

    First Commit (Mahmoud Nassim)

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="App w-full  flex justify-center">
+      <div className="container pt-4">
+        <h1>Welcome</h1>
+      </div>
+    </div>
+  );
+}
